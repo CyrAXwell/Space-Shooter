928786a88c583f3507a79237f2558c108fa5c820
CyrAXwell <pogrebnoy_y@mail.ru>
2024-07-29 13:22:37 +0300

    refactor: CharacterController

--- Assets/Scripts/Player/CharacterController.cs
+++ Assets/Scripts/Player/CharacterController.cs
@@ -1,79 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
+    [SerializeField] private float speed;
+    [SerializeField] private float acceleration;
 
-    [SerializeField] float speed;
-    private Vector2 direction;
-    private Rigidbody2D rb;
-    private Vector2 speedFunction;
-    [SerializeField] float acceleration;
+    private Vector2 _direction;
+    private Rigidbody2D _rb2D;
     public float currentSpeed = 0f;
 
-    void Start(){
-        rb = GetComponent<Rigidbody2D>();
+    void Start()
+    {
+        _rb2D = GetComponent<Rigidbody2D>();
     }
     
     void Update() {
-        direction.x = Input.GetAxisRaw("Horizontal");
-        direction.y = Input.GetAxisRaw("Vertical");
+        _direction.x = Input.GetAxisRaw("Horizontal");
+        _direction.y = Input.GetAxisRaw("Vertical");
         
-        direction.Normalize();
-        
-        // speedFunction +=  direction * acceleration * Time.deltaTime;
+        _direction.Normalize();
 
-        // if(Mathf.Abs(speedFunction.x) >= speed)
-        // {
-        //    speedFunction.x = direction.x * speed;
-        // }
-
-        // if(Mathf.Abs(speedFunction.y) >= speed)
-        // {
-        //    speedFunction.y = direction.y * speed;
-        // }
-
-
-        if(direction.magnitude != 0f)
+        if(_direction.magnitude > 0f)
         {
             currentSpeed += Time.deltaTime * acceleration;
-            if(currentSpeed >= speed)
-            {
-                currentSpeed = speed;
-            }
+            currentSpeed = currentSpeed > speed ? speed : currentSpeed;
         }
-
-        if(direction.magnitude == 0)
+        else
         {
             currentSpeed = 0;
         }
-
-        //Debug.Log("speed " + currentSpeed);
-        // Vector3 pos = transform.position;
-        // pos.y += Input.GetAxis("Vertical") * speed * Time.deltaTime;
-        // pos.x += Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-        // transform.position = pos;
     }
 
-    void FixedUpdate(){
-        
-        //rb.MovePosition(rb.position + direction * speed * Time.fixedDeltaTime);
-        // direction.x = Input.GetAxisRaw("Horizontal");
-        // direction.y = Input.GetAxisRaw("Vertical");
-
-        // direction.Normalize();
-        
-        rb.velocity = direction * currentSpeed;
-        //rb.velocity = speedFunction;
-        //rb.velocity = direction * speed;
-
-        // Vector3 pos = transform.position;
-        // pos.y += Input.GetAxis("Vertical") * speed * Time.fixedDeltaTime;
-        // pos.x += Input.GetAxis("Horizontal") * speed * Time.fixedDeltaTime;
-        // transform.position = pos;
-
-        
+    void FixedUpdate()
+    {
+        _rb2D.velocity = _direction * currentSpeed;
     }
 
 }
